05973d121da343626c03cb1f8eddcc4d342842ce
yashsingadiya1428-ux <yashsingadiya1428@gmail.com>
2026-07-03 13:34:36 +0530

    Initial MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Git.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
